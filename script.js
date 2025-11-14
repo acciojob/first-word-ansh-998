b35a9c585654eb480c35d073e20523f691ff78ad
Ansh Gangwar <anshgangwar998@gmail.com>
2025-11-14 20:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+  s = s.trimStart();
+
+  let word = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") break;
+    word += s[i];
+  }
+
+  return word;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
